a2b98e29b9c0e908057813e73a11d4cc8c3bd33e
David Barcenas <daveepro@outlook.com>
2021-11-01 17:07:39 -0600

    [flip-string] change loop method and change regex to preserve other characters

--- flip-string.js
+++ flip-string.js
@@ -8,19 +8,19 @@ function main(input) {
     throw new Error('The input parameter must be a string and cannot be empty.')
   }
 
+  // if you add or remove symbols, also do it in the regex.
+  const symbols = [',', '$', '!']
+  const regex = /[$!,]/g
+
   const word = input.trim()
   const convertWordToArray = word.split('')
-  const flipWord = word.replace(/\W/g, '').split('').reverse()
+  const flipWord = word.replace(regex, '').split('').reverse()
 
-  const symbols = [',', '$', '!']
-
-  for (let i = 0; i <= convertWordToArray.length; i++) {
-    let character = convertWordToArray[i]
-
+  convertWordToArray.reduce((_, character, i) => {
     if (symbols.includes(character)) {
       flipWord.splice(i, 0, character)
     }
-  }
+  })
 
   return flipWord.join('')
 }
@@ -33,7 +33,9 @@ function main(input) {
 const test1 = main("a,b$c") === "c,b$a"
 const test2 = main("Ab,c,de!$") === "ed,c,bA!$"
 const test3 = main("abcdef") === "fedcba"
+const test4 = main("Z&hk$6-#P,") === "P#-6$kh&Z,"
 
 console.log(test1)
 console.log(test2)
 console.log(test3)
+console.log(test4)
