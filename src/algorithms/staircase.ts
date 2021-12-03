24e0058523d398e47fad823cfa9e167a2226a3f1
David Barcenas <daveepro@outlook.com>
2021-12-03 12:57:41 -0600

    [challenge#3] minimum and maximum sum

--- src/algorithms/staircase.ts
+++ src/algorithms/staircase.ts
@@ -2,7 +2,6 @@
  * The staircase must have a base and a height equal to n
  */
 
-
 export function stairCase(n: number): string {
   const MIN_NUMBER = 0;
   const INITIAL_VALUE = 1;
@@ -14,9 +13,9 @@ export function stairCase(n: number): string {
   }
 
   for (let i = n - INITIAL_VALUE; i >= MIN_NUMBER; i--) {
-    str += ' '.repeat(i).concat('#'.repeat(n - i))
-    str += '\n'
+    str += ' '.repeat(i).concat('#'.repeat(n - i));
+    str += '\n';
   }
-  
+
   return str;
 }
