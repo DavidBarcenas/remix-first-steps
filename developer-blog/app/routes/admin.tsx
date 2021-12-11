be50bc0f88f4eb6ae7d408aa152942dae8b6dbce
David Barcenas <daveepro@outlook.com>
2021-12-11 17:22:31 -0600

    create admin section and nested routes

---
+++ developer-blog/app/routes/admin.tsx
@@ -0,0 +1,35 @@
+import { getPosts } from '~/posts';
+import { Link, Outlet, useLoaderData } from 'remix';
+
+import adminStyles from '~/styles/admin.css'
+import type { Post } from '../posts';
+
+export const links = () => {
+  return [{rel: 'stylesheet', href: adminStyles}]
+}
+
+export const loader = () => {
+  return getPosts()
+}
+
+export default function Admin() {
+  const posts = useLoaderData<Post[]>()
+
+  return (
+    <div className='admin'>
+      <nav>
+        <h1>Admin</h1>
+        <ul>
+          {posts.map(post => (
+            <li key={post.slug}>
+              <Link to={`/posts/${post.slug}`}>{post.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
